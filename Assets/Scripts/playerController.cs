6074b085d30beefe034e1996ddda172f9933dde2
LevelDownGirls <33215557+LevelDownGirls@users.noreply.github.com>
2019-09-01 19:12:23 +1000

    first commit

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    // define this in unity engine
+    public float movementSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey("w"))
+        {
+            transform.position += transform.TransformDirection(Vector3.forward) * Time.deltaTime * movementSpeed * 2.5f;
+        }
+        else if(Input.GetKey("w"))
+        {
+            transform.position += transform.TransformDirection(Vector3.forward) * Time.deltaTime * movementSpeed;
+        }
+    }
+}
---
+++ Assets/Scripts/worldGenerationController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class worldGenerationController : MonoBehaviour
+{
+    public GameObject wallPiece;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(wallPiece);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
