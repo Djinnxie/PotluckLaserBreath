4f369d8e605ad3e28be9c5c061836ce018fbf259
LevelDownGirls <33215557+LevelDownGirls@users.noreply.github.com>
2019-09-01 22:46:14 +1000

    built test hallways for procedural generation, tested placement and rotation

--- Assets/Scripts/worldGenerationController.cs
+++ Assets/Scripts/worldGenerationController.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class worldGenerationController : MonoBehaviour
 {
-    public GameObject wallPiece;
+    public GameObject corridor;
+    public GameObject tJunction;
+
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(wallPiece, new Vector3(0,0,0), Quaternion.identity);
-        Instantiate(wallPiece, new Vector3(0, 0, 8), Quaternion.identity);
+        //Instantiate(wallPiece, new Vector3(0,0,0), Quaternion.identity);
+        Instantiate(corridor, new Vector3(0, 0, 10), Quaternion.identity);
+        Instantiate(tJunction, new Vector3(0, 0, 20), Quaternion.identity);
+        Instantiate(corridor, new Vector3(16.25f, 0, 24.5f), Quaternion.Euler(0f, 90f, 0f));
         //wallPiece.transform.position = new Vector3(0, 0, 0);
     }
 
