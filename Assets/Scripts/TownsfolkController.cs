59c6a1c347c65cab358299bf3d8da4fb7f59f544
Valerie Kyrie <valerie.kyrie.valk@gmail.com>
2019-09-07 11:35:30 +1000

    Added AI, fixed UI scale

---
+++ Assets/Scripts/TownsfolkController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TownsfolkController : MonoBehaviour
+{
+    public GameObject bardObject;
+    public GameObject chefObject;
+    public GameObject farmerObject;
+
+    public int numberOfTownsfolk;
+    public GridMap gridMapScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SummonTownsfolk()
+    {
+        int randX, randY;
+
+        GameObject newTownsfolk;
+        GridSection currentGridSection;
+        for (int i = 0; i < numberOfTownsfolk; i++)
+        {
+            //replace with randomisation
+            newTownsfolk = chefObject;
+
+            do
+            {
+                randX = Random.Range(0, gridMapScript.outerGridSizeX);
+                randY = Random.Range(0, gridMapScript.outerGridSizeY);
+
+                currentGridSection = gridMapScript.GetGridSection(randX, randY);
+
+            } while (currentGridSection.coords[0] == -1);
+
+            Instantiate(newTownsfolk, currentGridSection.position, currentGridSection.rotation);
+        }
+    }
+}
---
+++ Assets/Scripts/TownsfolkEnemy.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class TownsfolkEnemy : MonoBehaviour
+{
+
+    private NavMeshAgent thisAgent;
+    private GameObject playerObject;
+    public int aggroDistance;
+    private Vector3 oldPLayerLocation;
+    private Vector3 newPlayerLocation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisAgent = GetComponent<NavMeshAgent>();
+        playerObject = GameObject.Find("Player");
+
+        newPlayerLocation = playerObject.transform.position;
+        oldPLayerLocation = newPlayerLocation;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        newPlayerLocation = playerObject.transform.position;
+
+        if (DistanceToPlayer() < aggroDistance)
+        {
+            thisAgent.SetDestination(newPlayerLocation);
+        }
+
+        oldPLayerLocation = newPlayerLocation;
+    }
+
+    public float DistanceToPlayer()
+    {
+        float disX = Mathf.Abs(transform.position.x - newPlayerLocation.x);
+        float disY = Mathf.Abs(transform.position.y - newPlayerLocation.y);
+
+        return disX + disY;
+    }
+}
