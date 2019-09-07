eaca78641ed89a587717d44839798f2d2ab03053
LevelDownGirls <33215557+LevelDownGirls@users.noreply.github.com>
2019-09-08 01:17:12 +1000

    Ragdolls + laser scrolling

---
+++ Assets/Scripts/scrollLaser.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrollLaser : MonoBehaviour
+{
+    public float scrollSpeed = 2.5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float offsetX = Time.time * - scrollSpeed;
+
+        GetComponent<Renderer>().material.mainTextureOffset = new Vector2(offsetX, 0);
+    }
+}
